2fd78c35203d968c855504e68c8959349511f64e
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2023-04-25 13:39:24 +0200

    Fixing missing `UsageTestAggregationCompatibilityRule` when used by regular JVM modules

--- plugin/src/main/kotlin/io/github/gmazzo/android/test/aggregation/UsageTestAggregationCompatibilityRule.kt
+++ plugin/src/main/kotlin/io/github/gmazzo/android/test/aggregation/UsageTestAggregationCompatibilityRule.kt
@@ -1,9 +1,12 @@
 package io.github.gmazzo.android.test.aggregation
 
+import org.gradle.api.Project
 import org.gradle.api.attributes.AttributeCompatibilityRule
+import org.gradle.api.attributes.AttributesSchema
 import org.gradle.api.attributes.CompatibilityCheckDetails
 import org.gradle.api.attributes.Usage
 import org.gradle.kotlin.dsl.USAGE_TEST_AGGREGATION
+import org.gradle.kotlin.dsl.add
 
 internal class UsageTestAggregationCompatibilityRule : AttributeCompatibilityRule<Usage> {
 
@@ -11,6 +14,21 @@ internal class UsageTestAggregationCompatibilityRule : AttributeCompatibilityRul
         if (consumerValue?.name == USAGE_TEST_AGGREGATION && producerValue?.name == Usage.JAVA_RUNTIME) {
             compatible()
         }
+    }
+
+    companion object {
+
+        fun bind(project: Project) {
+            bind(project.dependencies.attributesSchema)
+        }
+
+        fun bind(schema: AttributesSchema) {
+            schema
+                .attribute(Usage.USAGE_ATTRIBUTE)
+                .compatibilityRules
+                .add(UsageTestAggregationCompatibilityRule::class)
+        }
+
     }
 
 }
