9f7761a984f017167a5de53eedc8299505cadc0c
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2023-08-15 21:19:52 +0200

    Fixed required min sdk

--- demo-project/ui-tests/build.gradle.kts
+++ demo-project/ui-tests/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.app.test"
-    compileSdk = 33
+    compileSdk = libs.versions.android.sdk.get().toInt()
 
     targetProjectPath = projects.demoProject.app.dependencyProject.path
 
