a973decb3ae9d7665455b8977a89b067a234a52b
Guillermo Mazzola <gmazzo65@gmail.com>
2024-02-07 16:23:14 +0100

    Added basic tests for plugins

---
+++ plugin/src/test/kotlin/io/github/gmazzo/android/test/aggregation/AndroidTestResultsAggregationPluginTest.kt
@@ -0,0 +1,6 @@
+package io.github.gmazzo.android.test.aggregation
+
+class AndroidTestResultsAggregationPluginTest : AndroidTestBaseAggregationPluginTest(
+    pluginId = "io.github.gmazzo.test.aggregation.results",
+    expectedTask = "testAggregatedReport",
+)
