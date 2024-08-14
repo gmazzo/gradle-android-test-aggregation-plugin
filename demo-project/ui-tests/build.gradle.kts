fed60bb88c2c8dca569a0d5fe6be25a4bd82d376
Guillermo Mazzola <gmazzo65@gmail.com>
2024-08-14 14:41:17 +0200

    Fixed emulator on CI

--- demo-project/ui-tests/build.gradle.kts
+++ demo-project/ui-tests/build.gradle.kts
@@ -1,5 +1,3 @@
-import com.android.build.api.dsl.ManagedVirtualDevice
-
 plugins {
     alias(libs.plugins.android.test)
     alias(libs.plugins.android.baseline)
@@ -25,10 +23,10 @@ android {
     }
 }
 
-val emulator by android.testOptions.managedDevices.devices.creating(ManagedVirtualDevice::class) {
-    device = "Pixel 2"
-    apiLevel = libs.versions.android.compileSDK.get().toInt()
-    systemImageSource = "aosp_atd"
+val emulator by android.testOptions.managedDevices.localDevices.registering {
+    device = "Pixel 6"
+    apiLevel = 33
+    systemImageSource = "aosp"
 }
 
 baselineProfile {
