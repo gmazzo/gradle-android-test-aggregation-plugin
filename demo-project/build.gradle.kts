82cc9ad6b4f39809275c93b0a956f7dd3cbdfd81
Guillermo Mazzola <gmazzo65@gmail.com>
2024-11-28 15:39:20 +0100

    Added report content validation test (#199)

--- demo-project/build.gradle.kts
+++ demo-project/build.gradle.kts
@@ -1,3 +1,12 @@
+import com.github.difflib.DiffUtils
+import com.github.difflib.UnifiedDiffUtils
+
+buildscript {
+    dependencies {
+        classpath(libs.diffUtils)
+    }
+}
+
 plugins {
     base
     id("io.github.gmazzo.test.aggregation.results")
@@ -33,6 +42,59 @@ tasks.jacocoAggregatedCoverageVerification {
     }
 }
 
+val aggregatedReportsSpecs = layout.projectDirectory.dir("specs/aggregated-reports")
+
+tasks.jacocoAggregatedReport {
+    reports.csv.required = true
+}
+
+val reportsSpec = copySpec {
+    includeEmptyDirs = false
+    from(tasks.jacocoAggregatedReport) { include("**/*.csv") }
+    from(tasks.testAggregatedReport) {
+        into("tests")
+        filter { if (it.startsWith("<a href=\"http://www.gradle.org\">")) "" else it }
+    }
+}
+
+tasks.register<Sync>("collectExpectedReports") {
+    with(reportsSpec)
+    into(aggregatedReportsSpecs)
+}
+
+val checkAggregatedReportsContent by tasks.registering(Sync::class) {
+    with(reportsSpec) { into("actual") }
+    into(temporaryDir)
+    doLast {
+        fun File.collect() = walkTopDown()
+            .filter(File::isFile)
+            .associateBy { it.toRelativeString(this) }
+
+        val expected = File(temporaryDir, "expects").collect()
+        val actual = File(temporaryDir, "actual").collect()
+        val diff = (expected.keys + actual.keys).mapNotNull {
+            val expectedLines = expected[it]?.readLines().orEmpty()
+            val actualLines = actual[it]?.readLines().orEmpty()
+
+            when (actualLines) {
+                expectedLines -> null
+                else -> UnifiedDiffUtils.generateUnifiedDiff(
+                    "expected:${it}", "actual:${it}",
+                    expectedLines,
+                    DiffUtils.diff(expectedLines, actualLines),
+                    3
+                )
+            }
+        }
+        check(diff.isEmpty()) {
+            diff.joinToString(
+                prefix = "The generated reports are different than the expected ones:\n",
+                separator = "\n\n\n"
+            )
+        }
+    }
+}
+
 tasks.check {
-    dependsOn(tasks.jacocoAggregatedCoverageVerification)
+    dependsOn(tasks.jacocoAggregatedCoverageVerification, checkAggregatedReportsContent)
 }
